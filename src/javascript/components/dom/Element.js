02f211873d59c550b892a87eb23ea2d9b6888986
Eran <e.machiels@hotmail.com>
2017-03-17 12:49:44 +0100

    Added method to fetch attribute data

--- src/javascript/components/dom/Element.js
+++ src/javascript/components/dom/Element.js
@@ -1,4 +1,5 @@
 import Events from './Events.js';
+import Str from './../helpers/Str.js';
 
 class Element {
 
@@ -53,6 +54,43 @@ class Element {
 		return returnElements;
 	}
 
+    /**
+     * Returns the data attributes assigned to the element
+     *
+     * @example
+     * <div data-component="component" data-foo="bar" />
+     *
+     * @param {String} key (optional) The specific value of a attribute key that has to be returned
+     *
+     * @return {mixed} 	null: when not a specific element |
+     * 				   	String: when a key is specified |
+     * 					Object: when returning the whole options object
+     */
+    getData(key) {
+        if(!this.element || this.element.length) {
+            return null;
+        }
+
+        let returnData = {};
+        let i = 0
+
+        for(; i < this.element.attributes.length; i++) {
+            let attribute = this.element.attributes[i];
+
+            if(attribute.name.indexOf('data-') == 0) {
+                let name = attribute.name.substr("data-".length, attribute.name.length - 1);
+
+                returnData[name] = /{.*}/g.test(attribute.value) ? Str.toObject(attribute.value) : attribute.value;
+            }
+        }
+
+        if(key) {
+            return returnData[key] || "";
+        }
+
+        return returnData;
+    }
+
 	/**
 	 * Add an event to the element(s)
 	 *
@@ -76,26 +114,32 @@ class Element {
 		if( typeof this.element == "object" ) {
 			for( let i = 0; i < this.element.length; i++ ) {
 				if( typeof callback == 'function' ) {
-                    callback(this.element[i], i);
+                    callback(new Element(this.element[i]), i);
 				}
 			}
 		} else {
 			if( typeof callback == 'function') {
-                callback(this.element, 0);
+                callback(new Element(this.element), 0);
 			}
 		}
 	}
 
     /**
 	 * Gets the children for this element.
-     * @returns {number}
+     * @returns {Element}
      */
-    getChildren()
+    children( selector )
 	{
-
+		return new Element(selector);
 	}
 
-
+    /**
+     * Returns the height of the element
+     *
+     * @returns {number}
+     *
+     * @todo Improve options for height
+     */
 	getHeight() {
 		return this.element.clientHeight;
 	}
