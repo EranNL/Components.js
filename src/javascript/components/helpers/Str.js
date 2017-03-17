35e79b1fe2bc4599ce0c5f14aaf0d33f46beec76
Eran <e.machiels@hotmail.com>
2017-03-17 12:49:20 +0100

    Added string mutate methods

--- src/javascript/components/helpers/Str.js
+++ src/javascript/components/helpers/Str.js
@@ -10,6 +10,29 @@ class Str {
 
 		return words.join('');
 	}
+
+    /**
+	 * Returns the give string with a capitalized first letter
+     * @param {String} string
+     * @returns String
+     */
+	static ucFirst(string) {
+		return string.charAt(0).toUpperCase() + string.slice(1);
+	}
+
+    /**
+	 * Returns the given string as an object
+	 *
+	 * @return {Object}
+     */
+	static toObject() {
+        string = string.replace(/'/g, '"');
+        try {
+            return JSON.parse(string);
+        } catch (err) {
+            return {};
+        }
+	}
 }
 
 export default Str;
