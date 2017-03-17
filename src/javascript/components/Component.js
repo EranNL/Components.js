1ad9f0c1ff81843188224a92973374a9c3d1d19f
Eran <e.machiels@hotmail.com>
2017-03-17 16:05:54 +0100

    Added base class for components

---
+++ src/javascript/components/Component.js
@@ -0,0 +1,14 @@
+/**
+ *
+ */
+
+import Events from '/dom/Events';
+
+class Component {
+
+    constructor(element) {
+        this.element = element;
+
+        this.events = new Events(this);
+    }
+}
