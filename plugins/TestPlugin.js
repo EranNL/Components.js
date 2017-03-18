c5d59902eb429f4e04d1a285bc0350cf81f41711
Eran <e.machiels@hotmail.com>
2017-03-18 12:20:49 +0100

    Added testPlugin

---
+++ plugins/TestPlugin.js
@@ -0,0 +1,18 @@
+
+
+const TestPlugin = function (element) {
+    "use strict";
+    this.element = element;
+
+    //Register the custom event handler to this instance
+    OctaBootstrap.registerEvents(this);
+};
+
+TestPlugin.prototype = {
+    onClick: function () {
+        alert('this shit fucking works');
+    },
+
+};
+
+OctaBootstrap.registerComponent(TestPlugin, 'testplugin');
