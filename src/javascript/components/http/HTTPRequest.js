c8638d0029b909e2da706e4dd54107fd2e063d9c
Eran Machiels <e.machiels@hotmail.com>
2018-01-16 10:43:05 +0100

    Changed to Promise instead of own deferred. Code indents fix

--- src/javascript/components/http/HTTPRequest.js
+++ src/javascript/components/http/HTTPRequest.js
@@ -1,4 +1,3 @@
-import Promise from "../util/Promise";
 import HTTPResponse from "./HTTPResponse";
 import HTTPRequestException from "./exceptions/HTTPRequestException";
 import Str from "../util/Str";
@@ -8,48 +7,43 @@ class HTTPRequest {
     constructor(data) {
         this.data = data;
 
-        let deferred = new Promise();
-        let req = new XMLHttpRequest();
+        return new Promise((resolve, reject) => {
+            let req = new XMLHttpRequest();
 
-        req.open(this.data.method ? this.data.method.toUpperCase() : "GET", this.data.url ? this.data.url : "", this.data.async ? this.data.async : true);
+            req.open(this.data.method ? this.data.method.toUpperCase() : "GET", this.data.url ? this.data.url : "", this.data.async ? this.data.async : true);
 
-        if(this.data.method && this.data.method.toUpperCase() === 'POST') {
-            req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-        }
-
-        //Set the headers
-        if(this.data.headers) {
-            for(let prop in this.data.headers) {
-                req.setRequestHeader(prop, this.data.headers[prop]);
+            if (this.data.method && this.data.method.toUpperCase() === 'POST') {
+                req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
             }
-        }
 
-        if(this.data.responseType) {
-            req.setRequestHeader('Content-Type', 'application/' + this.data.responseType.toLowerCase());
-        }
-
-        req.onload = () => {
-            if(req.status === 200) {
-                deferred.resolve(new HTTPResponse(req));
+            //Set the headers
+            if (this.data.headers) {
+                for (let prop in this.data.headers) {
+                    req.setRequestHeader(prop, this.data.headers[prop]);
+                }
             }
-            else {
-                console.log(req.response);
-                deferred.reject(new HTTPRequestException("Ajax request couldn't be made"));
+
+            if (this.data.responseType) {
+                req.setRequestHeader('Content-Type', 'application/' + this.data.responseType.toLowerCase());
             }
-        };
 
-        // if(this.data.method.toUpperCase() === 'POST') {
-        //     req.send(Str.toURI(this.data.data));
-        // }
-        // else {
-            //req.send();
-        // }
+            if (!this.data.crossDomain) {
+                req.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+            }
 
-        this.req = req;
+            req.onload = () => {
+                if (req.status === 200) {
+                    resolve(new HTTPResponse(req));
+                }
+                else {
+                    reject(new HTTPResponse(req));
+                }
+            };
 
-        this.sendRequest();
+            this.req = req;
 
-        return deferred.promise();
+            this.sendRequest();
+        });
     }
 
     onStateChange() {
@@ -57,7 +51,7 @@ class HTTPRequest {
     }
 
     sendRequest() {
-        if(this.data.method && this.data.method.toUpperCase() === "POST") {
+        if (this.data.method && this.data.method.toUpperCase() === "POST") {
             this.req.send(Str.toURI(this.data.data));
         }
         else {
