812b1776b1586f5f51994f958b6052eaf147970f
Eran Machiels <e.machiels@hotmail.com>
2018-02-18 12:10:10 +0100

    Created Components

---
+++ src/javascript/framework/directives/FullEditorDirective.js
@@ -0,0 +1,15 @@
+import Component from "../../components/Component";
+
+class FullEditorDirective extends Component {
+    constructor(element) {
+        super(element);
+    }
+
+    init() {
+        CKEDITOR.replace( this.element.attr('id'), {
+            toolbar : 'Full',
+        });
+    }
+}
+
+export default FullEditorDirective;
---
+++ src/javascript/framework/directives/shop/CheckVoucherDirective.js
@@ -0,0 +1,29 @@
+import Directive from "../Directive";
+import HTTPRequest from "../../../components/http/HTTPRequest";
+import Element from "../../../components/dom/Element";
+
+class CheckVoucherDirective extends Directive {
+    constructor(element) {
+        super(element);
+    }
+
+    init() {
+        this.element.find('#check_voucher').on('click', () => this.onCheckVoucher());
+    }
+
+    onCheckVoucher() {
+        new HTTPRequest({
+            url: this.config.get('url') + '/shop/redeemvoucher/' + this.element.find('[name="voucher_code"]').get(0).val(),
+            method: 'GET'
+        })
+        .done((data) => {
+            data = data.toJSON();
+
+            let container = new Element("#voucher-content");
+
+            for(let part in data) {
+                container.append('<div></div>');
+            }
+        })
+    }
+}
